972bb269c049705004dca20e044dfb8d503efa10
Tianshi <56448258+TianshiWW@users.noreply.github.com>
2020-05-15 22:53:45 -0400

    Update db-config.js

--- server/db-config.js
+++ server/db-config.js
@@ -12,9 +12,6 @@
 // exports.setUpDatebase = setUpDatebase
 
 module.exports = {
-  // user          : "admin",
-  // password      : "mypassword",
-  // connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=cis550proj.cji2ugkx8gnr.us-east-1.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SID=ARTDB)))"
 
   host: "",
   user: "",
@@ -24,19 +21,4 @@ module.exports = {
 
 };
 
-// const host = "cis550proj.cvh2n4yqrctx.us-east-1.rds.amazonaws.com"
-// const user = "admin"
-// const password = "mypassword"
-// const database=  "MUSEUMDB"
-// const port = 1521
 
-// function setUpDatebase(callback) {
-//   var connection = mysql.createConnection({
-//       host: hostName,
-//       user: user,
-//       password: password,
-//       port: port,
-//       database: databaseName
-//   })
-//   callback(connection)
-// }
